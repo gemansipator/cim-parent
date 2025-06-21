fa29e1f306b8539643c5dc40f9a5740d94d05da6
gemansipator <gemansipator@gmail.com>
2025-06-21 18:57:25 +0300

    DTO для ответа при аутентификации.

---
+++ src/main/java/site/javatech/cim/core/dto/LoginResponse.java
@@ -0,0 +1,32 @@
+package site.javatech.cim.core.dto;
+
+import site.javatech.cim.core.model.User;
+
+/**
+ * DTO для ответа при аутентификации.
+ */
+public class LoginResponse {
+    private User user;
+    private String token;
+
+    public LoginResponse(User user, String token) {
+        this.user = user;
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
